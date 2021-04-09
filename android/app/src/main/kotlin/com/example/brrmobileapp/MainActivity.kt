6299048a71a42554ab80e0de4c8f9e558f5b50c3
manoj <manojkumar4152@gmail.com>
2021-04-09 11:31:02 +0530

    Brr Project Structure added

---
+++ android/app/src/main/kotlin/com/example/brrmobileapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.brrmobileapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This is the root widget of the application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BRR Mobile Application',
+      /*
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.yellowAccent,
+      ),
+
+       */
+      home: Splash(),
+
+    );
+  }
+}
